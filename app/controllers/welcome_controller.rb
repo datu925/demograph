062c14dcfbfb30419292133f59bb7cde5e4a372c
Dan Turcza <email": null>
2016-04-24 11:42:10 -0500

    Filter only to those who rsvp'd yes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -23,17 +23,18 @@ class WelcomeController < ApplicationController
       RSVP.new({
                 full_name: rsvp["member"]["name"],
                 first_name: rsvp["member"]["name"].split(" ").first,
-                is_attending: "null",
+                is_attending: rsvp["response"],
                 gender: "null"
               })
     end
 
     GenderDetector.detect_mass(@rsvps)
+    @attendees = @rsvps.select{ |rsvp| rsvp.is_attending == "yes" }
 
-    male = @rsvps.select { |rsvp| rsvp.gender == "male" }.length
-    female = @rsvps.select { |rsvp| rsvp.gender == "female" }.length
-    neutral = @rsvps.select { |rsvp| rsvp.gender == "not_guessed" }.length
-    total = @rsvps.length
+    male = @attendees.select { |attendee| attendee.gender == "male" }.length
+    female = @attendees.select { |attendee| attendee.gender == "female" }.length
+    neutral = @attendees.select { |attendee| attendee.gender == "not_guessed" }.length
+    total = @attendees.length
     ratio = female != 0 ? male / female.to_f : "Undefined (no female attendance"
     @stats = { male_percentage: male / total.to_f,
                female_percentage: female / total.to_f,
