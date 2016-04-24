04d5596f9fe693a4b86082b3561f13f2718bd9e7
Dan Turcza <email": null>
2016-04-24 14:44:20 -0500

    Add basic d3 chart; not sure if successful
    So I ran out of requests on genderize.io already... I hope that's just because of my inefficient testing yesterday. So I was not able to verify that the chart actually appears as desired and am just calling it for today until I get some more requests.
    
    Long-term strategy: save results of the API call to the database, so that you can only run the API call for names you've never predicted before.

---
+++ app/assets/javascripts/percent-chart.js
@@ -0,0 +1,43 @@
+$(function() {
+
+  var svgContainer = d3.select("#percent-chart")
+                       .append("svg")
+                       .attr("width", 500)
+                       .attr("height", 200);
+
+  var linearScale = d3.scale.linear()
+                      .domain([0,100])
+                      .range([50,450]);
+
+  var xAxis = d3.svg.axis()
+                    .scale(linearScale);
+
+  var circles = svgContainer.selectAll("circle")
+    .data([50])
+    .enter()
+    .append("circle")
+    .attr("cx", function(d) { return linearScale(d) })
+    .attr("cy", 75)
+    .attr("r", 10)
+    .style("fill", "black")
+
+  var xAxisGroup = svgContainer.append("g")
+                               .attr("transform", "translate(0, 100)")
+                               .call(xAxis)
+                               .style("stroke-width","1px");
+
+
+  svgContainer.append("text")
+              .attr("x", 75 )
+              .attr("y", 165 )
+              .style("text-anchor", "middle")
+              .text("all men");
+
+  svgContainer.append("text")
+              .attr("x", 425 )
+              .attr("y", 165 )
+              .style("text-anchor", "middle")
+              .text("all women");
+
+
+})
