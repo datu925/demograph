e21dee2d02d82fd4346afe8f263ef7024e871596
Dan Turcza <datu925@gmail.com>
2016-08-24 14:38:42 -0500

    Add Bootstrap and other styling
    Incorporated bootstrap, plus added gradient shading to the number line

--- app/assets/javascripts/percent-chart.js
+++ app/assets/javascripts/percent-chart.js
@@ -3,7 +3,7 @@ $(function() {
   var svgContainer = d3.select("#percent-chart")
                        .append("svg")
                        .attr("width", 500)
-                       .attr("height", 200);
+                       .attr("height", 100);
 
   var linearScale = d3.scale.linear()
                       .domain([0,100])
@@ -19,14 +19,36 @@ $(function() {
     .enter()
     .append("circle")
     .attr("cx", function(d) { return linearScale(d) })
-    .attr("cy", 75)
-    .attr("r", 10)
-    .style("fill", "black")
+    .attr("cy", 40)
+    .attr("r", 8)
+    .style("fill", "gray")
 
   var xAxisGroup = svgContainer.append("g")
-                               .attr("transform", "translate(0, 100)")
+                               .attr("transform", "translate(0, 50)")
+                               .attr("class", "axis")
                                .call(xAxis)
-                               .style("stroke-width","1px");
+                               .style("fill","url(#MyGradient)");
+
+  var gradient = svgContainer.append("svg:defs")
+              .append("svg:linearGradient")
+              .attr("id", "MyGradient")
+              // .attr("x1", "0%")
+              // .attr("y1", "0%")
+              // .attr("x2", "100%")
+              // .attr("y2", "100%")
+              .attr("spreadMethod", "pad");
+
+  // Define the gradient colors
+  gradient.append("svg:stop")
+      .attr("offset", "0%")
+      .attr("stop-color", "Pink")
+      .attr("stop-opacity", 1);
+
+  gradient.append("svg:stop")
+      .attr("offset", "100%")
+      .attr("stop-color", "DeepSkyBlue")
+      .attr("stop-opacity", 1);
+
 
 
   svgContainer.append("text")
