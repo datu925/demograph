77ff0383d0715687ebc78f0a54cf46de3428c614
Dan Turcza <datu925@gmail.com>
2016-08-24 11:32:38 -0500

    Remove email and notification features for Heroku
    In order to get something polished sooner rather than later, I'm taking down the email and notification features to concentrate on a quick styling reboot and having something classy on Heroku. Will put them up later when I am able to test them more thoroughly.

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,20 +1,20 @@
 class NotificationsController < ApplicationController
-  def create
-    start_time = Time.at(params[:event_time].to_i / 1000)
-    event = Event.find_or_create_by(url: params[:event_url], name: params[:event_name], start_date: start_time)
-    notification = Notification.find_or_create_by(event: event, user_id: session[:user_id], days_before: params[:notification][:days_before].to_i)
-    target_date = (event.start_date - notification.days_before.days).to_time
-    ScheduleNotificationJob.set(wait_until: target_date).perform_later(notification)
-    redirect_to notifications_path
-  end
+  # def create
+  #   start_time = Time.at(params[:event_time].to_i / 1000)
+  #   event = Event.find_or_create_by(url: params[:event_url], name: params[:event_name], start_date: start_time)
+  #   notification = Notification.find_or_create_by(event: event, user_id: session[:user_id], days_before: params[:notification][:days_before].to_i)
+  #   target_date = (event.start_date - notification.days_before.days).to_time
+  #   ScheduleNotificationJob.set(wait_until: target_date).perform_later(notification)
+  #   redirect_to notifications_path
+  # end
 
-  def index
-    @notifications = Notification.where(user_id: session[:user_id])
-  end
+  # def index
+  #   @notifications = Notification.where(user_id: session[:user_id])
+  # end
 
-  def destroy
-    Notification.find(params[:id]).destroy
-    redirect_to notifications_path
-  end
+  # def destroy
+  #   Notification.find(params[:id]).destroy
+  #   redirect_to notifications_path
+  # end
 
 end
